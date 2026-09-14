0951e80261fc4d9a7c693dfe91f94e1896f9dec0
ALFANEGOCIOS <softimuspro@gmail.com>
2026-09-14 13:24:26 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
+// Tu URL del proyecto (Project URL)
+const SUPABASE_URL = 'https://jlolyhglduixwjjngcuk.supabase.co'; 
+
+// Tu clave pública cliente (Publishable Key / Anon Key)
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_Tby5rC8Eap2YANn859wggg_5yjFLdMZ'; 
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
